09e4c1459b4579f87eab715c082af827f7927ed0
JNE JKL <juuso.jaakkola@gmail.com>
2021-02-24 00:56:04 +0200

    citta

---
+++ index.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+const clearConsole = require('clear-any-console');
+console.log('HAHA')
+clearConsole();
+
+console.log(`
+
+Juuso Jaakkola - Web Developer/Devaaja
+
+Juuso on laaja-alainen asioiden sisäistäjä ja totuuden etsijä. 
+Erityisiä kiinnostuksen kohteita ovat muinaiset viisausperinteet ja modernin tieteen löydöt. 
+Näiden hyvin erilaisten näkökantojen yhdistäminen ja ymmärtäminen antaa Juusolle 
+ainutlaatuisen perspektiivin katsoa dynaamista maailmaamme. Sähköoppi ja ohjelmointi ovat 
+antaneet Juusolle hyvät työkalut ymmärtämään maailmaamme. Näennäisen dualistusuuden läpi 
+näkeminen ja ykseyden ymmärtäminen arkisessa elämässä tuottaa Juusolle päivittäin nautintoa.
+Hänen kiinnostuksensa on vähintään yhtä paljon aineettomassa kuin aineellisessa maailmassa.
+
+Ammatilliset tavoitteet ovat lohkoketjuteknologiaa hyödyntävissä ratkaisuissa 
+(Bitcoin & Chainlink?!) ja elämän energian hallitsemisessa omaksi ja muiden hyödyksi. 
+
+
+Twitter: https://www.twitter.com/juusojaa
+
+
+
+`)
