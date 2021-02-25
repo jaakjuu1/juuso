847fa908695da846fbe730707fede3386bc886b0
JNE JKL <juuso.jaakkola@gmail.com>
2021-02-25 22:26:01 +0200

    nice bio

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
 #!/usr/bin/env node
 
 const clearConsole = require('clear-any-console');
+const pkgJSON = require('./package.json');
 
 clearConsole();
 
 console.log(`
+NAME: ${pkgJSON.name}
+VERSION: ${pkgJSON.version}
+DESCRIPTION: ${pkgJSON.description}
+`)
 
-Juuso Jaakkola - Web Developer/Devaajas
+console.log(`
+Juuso Jaakkola - Developer
 
 Juuso on laaja-alainen asioiden sisäistäjä ja totuuden etsijä. 
-Erityisiä kiinnostuksen kohteita ovat muinaiset viisausperinteet ja modernin tieteen löydöt. 
-Näiden hyvin erilaisten näkökantojen yhdistäminen ja ymmärtäminen antaa Juusolle 
-ainutlaatuisen perspektiivin katsoa dynaamista maailmaamme. Sähköoppi ja ohjelmointi ovat 
-antaneet Juusolle hyvät työkalut ymmärtämään maailmaamme. Näennäisen dualistusuuden läpi 
-näkeminen ja ykseyden ymmärtäminen arkisessa elämässä tuottaa Juusolle päivittäin nautintoa.
-Hänen kiinnostuksensa on vähintään yhtä paljon aineettomassa kuin aineellisessa maailmassa.
+Erityisiä kiinnostuksen kohteita ovat muinaiset viisausperinteet ja 
+modernin tieteen löydöt. Näiden erilaisten näkökantojen yhdistäminen 
+ja ymmärtäminen antaa Juusolle ainutlaatuisen perspektiivin katsoa 
+dynaamista maailmaamme.
+
+Utelias mieli, sähköoppi ja ohjelmointi ovat antaneet Juusolle avaimet,
+joilla sukeltaa maailmamme. Näennäisen dualistusuuden läpi näkeminen ja 
+ykseyden ymmärtäminen arkisessa elämässä tuottaa Juusolle ja hänen ympärilleen 
+päivittäin haasteita ja nautintoa. Hänen kiinnostuksensa on yhtä paljon 
+aineettomassa kuin aineellisessakin maailmassa.
 
 Ammatilliset tavoitteet ovat lohkoketjuteknologiaa hyödyntävissä ratkaisuissa 
-(Bitcoin & Chainlink?!) ja elämän energian hallitsemisessa omaksi ja muiden hyödyksi. 
+(Bitcoin & Chainlink?!) ja elämän energian hallitsemisessa omaksi ja muiden 
+hyödyksi. 
 
 
-Twitter: https://www.twitter.com/juusojaa
-
-
+Twitter: @juusojaa
+Telegram: @jne89
 
 `)
